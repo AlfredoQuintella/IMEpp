7ba1ddf3a3caa524551d27d2356b84aa609cd587
Alfredo Quintella <alfredo.quintella@gmail.com>
2024-02-27 14:24:00 -0300

    Create Crane_and_Turtle.cpp

---
+++ Homework0+/Crane_and_Turtle.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int X, Y;
+    cin >> X >> Y;
+    if ((Y + 1) % 2 && Y >= 2*X && Y <= 4*X) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
