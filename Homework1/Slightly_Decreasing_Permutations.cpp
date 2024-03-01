5766d62d62e97d0514a92c8359b95d72b1da717a
Alfredo Quintella <alfredo.quintella@gmail.com>
2024-03-01 09:00:24 -0300

    Create Slightly_Decreasing_Permutations.cpp

---
+++ Homework1/Slightly_Decreasing_Permutations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    for(int i = 0; i < k; i++){
+        cout << n - i << " ";
+    }
+    cout << 1 << " ";
+    for(int i = 2; i < n - k + 1 ; i++){
+        cout << i << " ";
+    }
+}
