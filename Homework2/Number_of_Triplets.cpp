f60389134cabae1a74e2b86dbee24a5e40c04356
Alfredo Quintella <alfredo.quintella@gmail.com>
2024-03-12 21:43:38 -0300

    Create Number_of_Triplets.cpp

---
+++ Homework2/Number_of_Triplets.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n, a, b, contador = 0, x, y;
+    set<pair<int,int>> list;
+    set<pair<int,int>> ::iterator it1;
+    set<pair<int,int>> ::iterator it2;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        list.insert(make_pair(a,b));
+    }
+
+    for (auto it1 : list){
+        for (auto it2 : list){
+            if (it1 != it2){
+                x = it1.first + it2.first;
+                y = it1.second + it2.second;
+
+                if (x % 2 == 0 && y % 2 == 0){
+                    if (list.find(make_pair(x/2, y/2)) != list.end()){
+                        contador++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << contador/2;
+}
